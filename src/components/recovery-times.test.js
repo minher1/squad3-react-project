9d19f4919c8be7280c0fdd386a5ddad7b24774f8
mhamedbenjmaa <mhamed.benjmaa@ibm.com>
2021-09-02 13:14:22 -0400

    add MTTR with formatting

--- src/components/recovery-times.test.js
+++ src/components/recovery-times.test.js
@@ -15,6 +15,23 @@ test('allow users to add recovery times', () => {
     userEvent.click(screen.getByRole("button"));
 
     expect(screen.getByText("09/01/2021 10:45:00 PM")).toBeVisible();
-    expect(screen.getByText(/15/)).toBeVisible();
+    expect(screen.getByText(/^15$/)).toBeVisible();
     expect(linkElement).toBeInTheDocument();
+});
+
+test('calculate MTTR', () => {
+    render(< RecoveryTimes />);
+    const startDateField = screen.getByLabelText("Start Date");
+    const startTimeField = screen.getByLabelText("Start Time");
+    const durationField = screen.getByLabelText("Duration");
+    const linkElement = screen.getByText(/Recovery Times/i);
+    userEvent.type(startDateField, "2021-09-01");
+    userEvent.type(startTimeField, "2245");
+    userEvent.type(durationField, "15");
+    userEvent.click(screen.getByRole("button"));
+    userEvent.type(startDateField, "2021-09-01");
+    userEvent.type(startTimeField, "2245");
+    userEvent.type(durationField, "25");
+    userEvent.click(screen.getByRole("button"));
+    expect(screen.getByText(/^20/)).toBeVisible();
 });
