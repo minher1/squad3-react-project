338f53e6d3403eef5395dcec30d01dc479088afd
Maxime Pavard <maxime.pavard@ibm.com>
2021-09-02 10:56:03 -0400

    Add deployment component with some style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
 import RecoveryTimes from "./components/recovery-times.js";
+import AddDeployement from './components/add-deployment';
+import { Col, Container, Row, Navbar } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <RecoveryTimes />
+    <div>
+      <Navbar bg="light">
+        <Container>
+          <Navbar.Brand>Squad 3 is awesome!</Navbar.Brand>
+        </Container>
+      </Navbar>
+      <Container>
+        <Row>
+          <Col>
+            <AddDeployement />
+          </Col>
+          <Col>
+            <RecoveryTimes />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -4,5 +4,7 @@ import App from './App';
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/Recovery Times/i);
+  const secondElement = screen.getByText(/Deployments/i);
   expect(linkElement).toBeInTheDocument();
+  expect(secondElement).toBeInTheDocument();
 });
